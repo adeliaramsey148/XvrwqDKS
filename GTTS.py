1d420ba11d2035b24a54bdf7a5656dc19880e699
adeliaramsey148 <104143460+adeliaramsey148@users.noreply.github.com>
2022-06-21 12:28:50 +0300

    Update GTTS.py

--- GTTS.py
+++ GTTS.py
@@ -3,10 +3,8 @@ from gtts import gTTS
 
 class GTTS:
     def tts(
-        self,
         req_text: str = "Google Text To Speech",
         filename: str = "title.mp3",
-        random_speaker=False,
         censor=False,
     ):
         tts = gTTS(text=req_text, lang="en", slow=False)
